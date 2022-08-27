72793c2931a2d79beae34b005ad0ebce413e880a
Yusif Bayramov <byusub2@gmail.com>
2022-08-28 00:52:09 +0400

    video pause responsive edit

--- js/main.js
+++ js/main.js
@@ -1,53 +1,52 @@
-let preveiwContainer = document.querySelector('.products-preview');
-let previewBox = preveiwContainer.querySelectorAll('.preview');
+let preveiwContainer = document.querySelector('.products-preview')
+let previewBox = preveiwContainer.querySelectorAll('.preview')
 
-document.querySelectorAll('.menu-item').forEach(product => {
+document.querySelectorAll('.menu-item').forEach((product) => {
     product.onclick = () => {
-        preveiwContainer.style.display = 'flex';
-        let name = product.getAttribute('data-name');
-        previewBox.forEach(preview => {
-            let target = preview.getAttribute('data-target');
+        preveiwContainer.style.display = 'flex'
+        let name = product.getAttribute('data-name')
+        previewBox.forEach((preview) => {
+            let target = preview.getAttribute('data-target')
             if (name == target) {
-                preview.classList.add('active');
+                preview.classList.add('active')
             }
-        });
+        })
+    }
+})
 
-    };
-
-});
-
-previewBox.forEach(close => {
+previewBox.forEach((close) => {
     close.querySelector('.fa-times').onclick = () => {
-        close.classList.remove('active');
-        preveiwContainer.style.display = 'none';
-    };
-});
+        close.classList.remove('active')
+        preveiwContainer.style.display = 'none'
+    }
+})
 window.onclick = function(event) {
-    let preveiwContainer = document.querySelector('.products-preview');
+    let preveiwContainer = document.querySelector('.products-preview')
     if (event.target == preveiwContainer) {
-        previewBox.forEach(close => {
-            close.classList.remove('active');
-            preveiwContainer.style.display = 'none';
-
-        });
+        previewBox.forEach((close) => {
+            close.classList.remove('active')
+            preveiwContainer.style.display = 'none'
+        })
     }
-}
-
-const vidoePlayBtn = document.querySelector(".img2")
-const video = document.querySelector(".vidoe-container");
-const closevideo = document.querySelector(".closevideo");
-
-vidoePlayBtn.addEventListener("click", () => {
-    video.classList.add("active");
-})
-
-closevideo.addEventListener("click", () => {
-    video.classList.remove("active");
-})
-
-window.onclick = function(event) {
-
     if (event.target == video) {
-        video.classList.remove("active");
+        manqalVideo.pause()
+        video.classList.remove('active')
+        manqalVideo.classList.remove('active')
     }
 }
+
+const vidoePlayBtn = document.querySelector('.img2')
+const video = document.querySelector('.vidoe-container')
+const closevideo = document.querySelector('.closevideo')
+const manqalVideo = document.querySelector('.manqal-video')
+
+vidoePlayBtn.addEventListener('click', () => {
+    manqalVideo.classList.add('active')
+    video.classList.add('active')
+})
+
+closevideo.addEventListener('click', () => {
+    manqalVideo.pause()
+    video.classList.remove('active')
+    manqalVideo.classList.remove('active')
+})
